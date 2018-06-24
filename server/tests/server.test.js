9d0dd21a3ebeb67dcf3ddca19f73dae748c46358
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-06-24 12:37:05 +1000

    Updated tests for /server

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,8 +8,16 @@ const {
 	Todo
 } = require('./../models/todo');
 
+const todos = [{
+	text: "First test todo"
+}, {
+	text: "Second test todo"
+}];
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,12 +34,11 @@ describe('POST /todos', () => {
 				expect(res.body.text).toBe(text);
 			})
 			.end((err, res) => {
-				if (err) {
-					done(err);
-					return;
-				}
+				if (err) return done(err);
 
-				Todo.find().then((todos) => {
+				Todo.find({
+					text
+				}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -45,15 +52,24 @@ describe('POST /todos', () => {
 			.send({})
 			.expect(400)
 			.end((err, res) => {
-				if (err) {
-					done(err);
-					return;
-				}
+				if (err) return done(err);
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((e) => done(e));
 			});
+	});
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 });
